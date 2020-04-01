1f700c53920931eee8fb22ec7fb23c497e8afdd7
Henri Bergius <henri.bergius@iki.fi>
2020-04-01 19:38:07 +0200

    Add knobs for easy playing with values in storybook

--- src/components/DashboardValue.stories.jsx
+++ src/components/DashboardValue.stories.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
+import { withKnobs, select, number } from '@storybook/addon-knobs';
 import DashboardValue from './DashboardValue';
 
 export default {
   title: 'Dashboard value',
   component: DashboardValue,
+  decorators: [withKnobs],
 };
-export const Basic = () => (
-  <DashboardValue
-    label="SOG"
-    value={3.3}
-    unit="m/s"
-    desiredUnit="kt"
-  />
-);
+
+const unitOptions = [
+  {
+    label: 'SOG',
+    unit: 'm/s',
+    desiredUnit: 'kt',
+  },
+  {
+    label: 'Temp',
+    unit: 'kelvin',
+    desiredUnit: 'temp-C',
+  },
+  {
+    label: 'Baro',
+    unit: 'Pa',
+    desiredUnit: 'hPa',
+  },
+];
+
+export const Basic = () => {
+  const units = select('Unit', unitOptions, unitOptions[0]);
+  return (
+    <DashboardValue
+      label={units.label}
+      value={number('Value', 3.3)}
+      unit={units.unit}
+      desiredUnit={units.desiredUnit}
+    />
+  );
+};
 
 export const WithoutValue = () => (
   <DashboardValue
