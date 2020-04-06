f5cb1431ed7f8a7d4f77a3ce111b09ff7cb2c319
Henri Bergius <henri.bergius@iki.fi>
2020-04-06 21:14:58 +0200

    Add gybe angles as well

--- src/components/WindDisplay.jsx
+++ src/components/WindDisplay.jsx
@@ -18,14 +18,17 @@ function WindDisplay(props) {
   const { bearing, apparentWind } = props;
   const rotation = 90 + bearing;
   let currentOffset = 0;
+  // Note: these should be read from polars
   const beatAngle = 42;
+  const gybeAngle = 27;
   const getCoordinatesForDegrees = (degrees) => {
     const angle = 2 * Math.PI * (degrees / 360);
     const x = (radius + 10) + (Math.cos(angle) * radius);
     const y = (radius + 14) + (Math.sin(angle) * radius);
     return [x, y];
   };
-  const windRotation = -90 - beatAngle + apparentWind;
+  const beatRotation = -90 - beatAngle + apparentWind;
+  const gybeRotation = -90 - gybeAngle + apparentWind + 180;
   return (
     <svg width={radius * 2 + 20} height={radius * 2 + 20}>
       <circle
@@ -49,8 +52,21 @@ function WindDisplay(props) {
         stroke="red"
         strokeWidth={0}
         fill="red"
-        fillOpacity={0.6}
-        transform={`rotate(${windRotation} ${radius + 10} ${radius + 14})`}
+        fillOpacity={0.4}
+        transform={`rotate(${beatRotation} ${radius + 10} ${radius + 14})`}
+      />
+      <path
+        d={`
+          M ${radius + 10} ${radius + 14}
+          L ${radius + 10 + radius} ${radius + 14}
+          A ${radius} ${radius} 0 0 1 ${getCoordinatesForDegrees(gybeAngle * 2)[0]} ${getCoordinatesForDegrees(gybeAngle * 2)[1]}
+          Z
+        `}
+        stroke="red"
+        strokeWidth={0}
+        fill="red"
+        fillOpacity={0.2}
+        transform={`rotate(${gybeRotation} ${radius + 10} ${radius + 14})`}
       />
       <path
         d="
