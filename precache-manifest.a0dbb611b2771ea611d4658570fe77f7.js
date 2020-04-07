d905c85f5597623406171703c1ed1d6f0000695d
Henri Bergius <henri.bergius@iki.fi>
2020-04-07 13:55:58 +0000

    Deploy meri-imperiumi/webapps to github.com/meri-imperiumi/webapps.git:gh-pages

---
+++ precache-manifest.a0dbb611b2771ea611d4658570fe77f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f19d86a0a46b9f30939db8a60d164c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a902cd92821104cf46b1.bundle.js"
+  },
+  {
+    "url": "runtime~main.a902cd92821104cf46b1.bundle.js"
+  },
+  {
+    "revision": "cf70ea5c6a5381005303",
+    "url": "static/css/main.df20520a.chunk.css"
+  },
+  {
+    "revision": "6864287b0b52ed5d8b9fa6d722659002",
+    "url": "static/media/Eurostile.6864287b.woff"
+  },
+  {
+    "revision": "db842f76ac5c59c01d5882570393f4ac",
+    "url": "static/media/nasalization-rg.db842f76.woff"
+  },
+  {
+    "url": "vendors~main.a902cd92821104cf46b1.bundle.js"
+  },
+  {
+    "url": "vendors~main.a902cd92821104cf46b1.bundle.js.LICENSE.txt"
+  }
+]);
